59939535781c55628e40b266b20ccaac95352b1d
DustyFlower <lukashevanatalia1992@gmail.com>
2023-12-06 16:16:01 +0300

    Task is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,140 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Country} from './Country';
+import axios from 'axios';
 
-export type BanknotsType = 'Dollars' | 'RUBLS' | 'All'
-export type MoneyType = {
-    banknotes: BanknotsType
-    value: number
-    number: string
-}
-
-let defaultMoney: MoneyType[] = [
-    {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
-    {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
-    {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
-    {banknotes: 'RUBLS', value: 100, number: ' r1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
-    {banknotes: 'RUBLS', value: 50, number: ' v1234567890'},
-]
-export const moneyFilter = (money: MoneyType[], filter: BanknotsType) => {
-    if (filter === 'All') {
-        return defaultMoney
+type PropsType =
+    {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean
     }
-    return money.filter(el => el.banknotes === filter)
-}
 
 function App() {
-    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
-    const [filterValue, setFilterValue] = useState<BanknotsType>('All')
 
-    const filteredMoney = moneyFilter(money, filterValue)
+
+    const [todos, setTodos] = useState<Array<PropsType>>([])
+    const axiosRequest = () => {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then((res) => {
+                setTodos(res.data)
+            })
+    }
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }, [])
+
+    const onClickHandler = () => {
+        setTodos([])
+    }
+
+    const mapTodos = todos.map(el => {
+        return (
+            <li><span>{el.id}</span><span>{el.title}</span><span>{el.completed}</span></li>
+        )
+    })
+
+    function onClickHandlerForRedisplay() {
+        axiosRequest()
+    }
 
     return (
         <div className="App">
-            <Country
-                data={filteredMoney}   //отрисовать будем деньги после фильтрации
-                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
-
-            />
+            <button onClick={onClickHandler}>CLEAN POSTS</button>
+            <button onClick={onClickHandlerForRedisplay}>REDISPLAY</button>
+            <ul>
+                {mapTodos}
+            </ul>
         </div>
     );
 }
 
-// Итого: в этой компоненте у нас мозги. А вот отрисовка где-то глубже. Погружаемся в Country
-
-
 export default App;
+
+
+//----------------------------------------------------------------------------------------
+
+// import React, {useEffect, useState} from 'react';
+// import './App.css';
+// import axios from "axios";
+//
+//
+// type PropsType =
+//     {
+//         userId: number,
+//         id: number,
+//         title: string,
+//         completed: boolean
+//     }
+//
+// function App() {
+//     const [todos, setTodos] = useState<Array<PropsType>>([])
+//
+//     const axiosRequest=()=>{
+//         axios.get('https://jsonplaceholder.typicode.com/todos')
+//             .then((res) => {
+//                 setTodos(res.data)
+//             })
+//     }
+//
+//     useEffect(() => {
+//         // fetch('https://jsonplaceholder.typicode.com/todos')
+//         //     .then(response => response.json())
+//         //     .then(json => setTodos(json))
+//
+//         // axios.get('https://jsonplaceholder.typicode.com/todos')
+//         //     .then((res) => {
+//         //         setTodos(res.data)
+//         //     })
+//
+//         axiosRequest()
+//     }, [])
+//
+//     const mapTodos=todos.map(el=>{
+//         return (
+//             <li>
+//                 <span>{el.id} - </span>
+//                 <span>{el.title}</span>
+//                 <span>{el.completed}</span>
+//             </li>
+//         )
+//     })
+//
+//     const onClickHandler = () => {
+//         setTodos([])
+//     }
+//
+//     const onClickHandlerForRedisplay=()=>{
+//         // axios.get('https://jsonplaceholder.typicode.com/todos')
+//         //     .then((res) => {
+//         //         setTodos(res.data)
+//         //     })
+//
+//         axiosRequest()
+//     }
+//
+//     return (
+//         <div className="App">
+//             <button onClick={onClickHandler}>CLEAN POSTS</button>
+//             <button onClick={onClickHandlerForRedisplay}>REDISPLAY</button>
+//             <ul>
+//                 {/*{todos.map(el => {*/}
+//                 {/*    return (*/}
+//                 {/*        <li>*/}
+//                 {/*            <span>{el.id} - </span>*/}
+//                 {/*            <span>{el.title}</span>*/}
+//                 {/*            <span>{el.completed}</span>*/}
+//                 {/*        </li>*/}
+//                 {/*    )*/}
+//                 {/*})}*/}
+//
+//                 {mapTodos}
+//             </ul>
+//         </div>
+//     );
+// }
+//
+// export default App;
